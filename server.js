c42a70613c9bc1c97aff011980c8b3552e8f079b
Javier Velasco <javier.velasco86@gmail.com>
2012-10-18 11:57:22 +0200

    Change template engine to Jade

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ var app = express();
 app.configure(function(){
   app.set('port', process.env['app_port'] || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set('view engine', 'jade');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
